e33eb16611ef45cb61ae9c8e189264972dd01b59
paololais <paolo.lais1@gmail.com>
2025-07-08 17:08:38 +0200

    prova hardcodata

--- main.c
+++ main.c
@@ -51,7 +51,7 @@ int main(void) {
     while(1){
         if(PWM_state == 1){
             OC1R = 0;
-            OC2R = 2000;
+            OC2R = 3600;
             OC3R = 0;
             OC4R = 7200;
         } else {
